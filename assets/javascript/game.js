3c53095ece20f2213356f6c046bbf0b4ed0cd635
Mtnman <billman.designs@gmail.com>
2018-11-05 22:58:19 -0500

    Mon night

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,61 @@
-//
+//VARIABLES
+//===============================================================================================
 
-// var word = ["Bulliet",
-//     "Jim Beam",
-//     "Knob Creek",
-//     "Buffalo Trace",
-//     "Wild Turkey"
-// ]
-
-var quote = ["Never delay kissing a pretty girl or opening a bottle of whiskey. – Ernest Hemingway",
-    "“Civilization begins with distillation. – William Faulkner",
-    "I have never in my life seen a Kentuckian who didn’t have a gun, a pack of cards, and a jug of whiskey. – Andrew Jackson",
-    "Always drink your whiskey with your gun hand, to show your friendly intentions. – Scottish Klondiker’s Proverb",
-    "Too much of anything is bad, but too much good whiskey is barely enough. – Mark Twain"
-]
-
-var wordElement = document.getElementById("display_letters");
-
-
-var wins = [];
-
-// var remainingGuesses = 12;
-// var correctGuesses = [];
-// var wrongGuesses = [];
-
-function startGame() {
-
-    var word = ["Bulliet",
+//Array containing hidden words.
+var word = ["Bulliet",
     "Jim Beam",
     "Knob Creek",
     "Buffalo Trace",
     "Wild Turkey"
-    ];
-    var remainingGuesses = 12;
-    var correctGuesses = [];
-    var wrongGuesses = [];
-]
+];
 
-    //This for-loop places the word from the array into the document.
-    for (var i = 0; i < word.length; i++) {
-        correctGuesses.push("_");
+
+//Array containing quotes to display after word is guessed correctly
+var quote = ["Never delay kissing a pretty girl or opening a bottle of whiskey. – Ernest Hemingway",
+    "Civilization begins with distillation. – William Faulkner",
+    "I have never in my life seen a Kentuckian who didn’t have a gun, a pack of cards, and a jug of whiskey. – Andrew Jackson",
+    "Always drink your whiskey with your gun hand, to show your friendly intentions. – Scottish Klondiker’s Proverb",
+    "Too much of anything is bad, but too much good whiskey is barely enough. – Mark Twain"
+];
+
+//Place holders for wins and guesses
+var wins = [];
+var remainingGuesses = 12;
+var correctGuesses = [];
+var wrongGuesses = [];
+
+
+//FUNCTIONS
+//===============================================================================================
+
+//This function loops through each array
+function loopArray(list) {
+    for (var i = 0; i < list.length; i++) {
+        console.log(list[i]); //placing array info into console to prove is working
     }
-
-    wordElement.innerHTML = correctGuesses.join("");
-
 }
 
-startGame ();
+//Testing loopArray
+loopArray(word);
+
+//Need to figure out the length 
+
+
+
+
+//GEARS TO THE GAME
+//===============================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
 
 
